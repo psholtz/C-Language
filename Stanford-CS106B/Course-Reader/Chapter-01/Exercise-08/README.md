688eaa9a13581414dc62425a964a061e05138322
psholtz <paul.sholtz@gmail.com>
2012-05-18 13:46:01 -0700

    updating C++

---
+++ Stanford-CS106B/Course-Reader/Chapter-01/Exercise-08/README.md
@@ -0,0 +1,10 @@
+Exercise 8
+---------- 
+
+Using the **digitSum** function from the section entitled "The **while** statement" as a model, write a program that reads in an integer and then displays the number that has the same digits in the reverse order, as illustrated by this sample run:
+
+<pre>
+This program reverses the digits in an integer.
+Enter a positive integer: 123456789
+The reversed integer is: 987654321
+</pre>
---
+++ Stanford-CS106B/Course-Reader/Chapter-01/Exercise-08/main.cpp
@@ -0,0 +1,54 @@
+//
+//  main.cpp
+//  Exercise 1.8
+//
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#define SENTINEL "0"
+
+using namespace std;
+
+#pragma mark -
+#pragma mrk Declarations
+string getLine(const string&);
+int reverse(int,size_t);
+int reverse_recur(int,int,size_t);
+
+#pragma mark -
+#pragma mark Definitions
+string getLine(const string &prompt)
+{
+    cout << prompt;
+    string line;
+    cin >> line;
+    return line;
+}
+
+int reverse(int n,size_t len)
+{
+    return reverse_recur(n,0,len);
+}
+
+int reverse_recur(int n, int k, size_t len)
+{
+    return n < 10 ? n : pow(10,len-k-1) * (n%10) + reverse_recur(n/10,k+1,len);
+}
+
+#pragma mark -
+#pragma mark Driver Loop
+int main (int argc, const char * argv[])
+{
+    cout << "This program reverses the digits in an integer." << endl;
+    while ( true ) {
+        string number = getLine("Enter a positive integer: ");
+        if ( number == SENTINEL ) break;
+        int value1 = atoi(number.c_str());
+        int value2 = reverse(value1,number.length());
+        cout << "The reversed integer is " << value2 << endl;
+    }
+    
+    return 0;
+}
