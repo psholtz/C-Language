adb1a19afdf1d50f8523852c7c67e4ca5a368cc5
psholtz <paul.sholtz@gmail.com>
2012-06-04 11:00:43 -0700

    updating C++

---
+++ Stanford-CS106B/Course-Reader/Chapter-04/Exercise-03/LongestLine.cpp
@@ -0,0 +1,73 @@
+/*
+ * File: WindChill.cpp
+ * ---------------------- 
+ * Display a wind chill table like that provided by the National Weather Service.
+ */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+// Stanford C++ Libraries
+#include "console.h"
+#include "simpio.h"
+
+#define SENTINEL ""
+
+using namespace std;
+
+#pragma mark -
+#pragma mark Function Prototypes
+void readFile(string filename);
+
+#pragma mark -
+#pragma mark Function Definitions
+void readFile(string filename)
+{
+    string longestString = "", tmp = "";
+    stringstream buffer;
+    ifstream infile;
+    infile.open(filename.c_str());
+    if ( infile.fail() ) 
+    {
+        cout << "Could not find file: " << filename << endl;
+    }
+    else 
+    {
+        int ch;
+        while ( (ch = infile.get()) != EOF ) 
+        {
+            if ( ch == '\n' ) 
+            {
+                tmp = buffer.str();
+                if ( tmp.length() > longestString.length() ) 
+                {
+                    longestString = tmp;
+                }
+                buffer.str(std::string());
+            }
+            else 
+            {
+                buffer << (char)ch;
+            }
+        }
+        cout << "The longest string was: " << longestString << endl;
+    }
+    infile.close();
+}
+
+#pragma mark -
+#pragma mark Main Driver Loop
+int main() {
+    
+    string filename;
+    while ( true ) {
+        filename = getLine("Enter a filename: ");
+        if ( filename == SENTINEL ) break;
+        readFile(filename);
+        cout << endl;
+    }
+    
+    return 0;
+}
---
+++ Stanford-CS106B/Course-Reader/Chapter-04/Exercise-03/README.md
@@ -0,0 +1,10 @@
+Exercise 3
+---------- 
+
+Write a program that prints out the longest line in a file entered by the user. If several lines are all equally long, your program should print out the first such line.
+
+---
+
+Note: This program requires linking with the Stanford C++ Libraries:
+
+http://www.stanford.edu/class/cs106b/materials/cppdoc/
